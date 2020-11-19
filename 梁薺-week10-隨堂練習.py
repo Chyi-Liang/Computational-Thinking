c267fc57ebfd001dc98bbc058a10c1ee06e15feb
Chyi-Liang <71420090+Chyi-Liang@users.noreply.github.com>
2020-11-19 11:19:56 +0800

    week10-課程隨堂作業

---
+++ 梁薺-week10-隨堂練習.py
@@ -0,0 +1,10 @@
+movie_title = "taken"
+release_year = "2008"
+movie_time_min = 93
+movie_type = "action"
+
+
+print("電腦英文名稱",movie_title)
+print("上映時間",release_year)
+print("片長",movie_time_min)
+print("電影類型",movie_type)
